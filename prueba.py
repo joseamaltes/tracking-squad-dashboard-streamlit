f1e01565f55508cab8f8a8ad0bacdebf0b6d1a55
Jose Antonio <josea_458@hotmail.com>
2022-01-12 17:05:49 -0600

    Completar dashboard utilizando data dummy

--- prueba.py
+++ prueba.py
@@ -2,92 +2,23 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 from bokeh.plotting import figure
-import psycopg2
+#import psycopg2
 import sys 
 
-#Parameters that we need to connect to the POSTGRESQL DB
 
-param_dict = {
-    "host"      : "",
-    "database"  : "",
-    "user"      : "",
-    "password"  : "",
-    "port":5432
-}
-
-#Methods to establish connection 
-
-def connect(params_dic):
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params_dic)
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        sys.exit(1) 
-    print("Connection successful")
-    return conn
-
-def postgresql_to_dataframe(conn, select_query, column_names):
-    """
-    Tranform a SELECT query into a pandas dataframe
-    """
-    cursor = conn.cursor()
-    try:
-        cursor.execute(select_query)
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error: %s" % error)
-        cursor.close()
-        return 1
-    
-    # Naturally we get a list of tupples
-    tupples = cursor.fetchall()
-    cursor.close()
-    
-    # We just need to turn it into a pandas dataframe
-    df = pd.DataFrame(tupples, columns=column_names)
-    return df
-
-
-#Connection to the DB 
-
-#conexion1 = connect(param_dict)
-column_names = ['id', 'genero']
-postgresql_query = 'SELECT * FROM community.gender_table'
-
-#df_sql = postgresql_to_dataframe(conexion1, postgresql_query, column_names)
-#show_gender = (df_sql.groupby('genero').count() / df_sql.groupby('genero').count().sum()) * 100
-#st.write('THIS CHART IS OBTAINED FROM A REMOTE POSTGRESQL DB!!')
-#st.bar_chart(data=show_gender)
-
-
-st.write("""
-#HELLO WORLD!! 
-This is the first app in the community trackins squad using Streamlit
+st.title("""
+GetHired Community dashboard
 """)
 
-#Using the same dataset but loaded manually 
 
-serie_prueba = pd.read_csv('gender_mock_data.csv')
-dataP = (serie_prueba.groupby('genero').count() / serie_prueba.groupby('genero').count().sum()) * 100
 
-st.write('show a dataset')
-st.write(serie_prueba)
-st.write('Using streamlit charts')
-st.bar_chart(data=dataP)
+#Data visualization using bokeh
 
-mock_data_df = pd.read_csv('USERS_MOCK_DATA_1.csv')
-mock_data_df['creado_dia'] = pd.to_datetime(mock_data_df['creado_dia'])
+df_users  = pd.read_csv('USERS_MOCK_DATA_1.csv')
+df_users['creado_dia'] = pd.to_datetime(df_users['creado_dia'])
 
-y = mock_data_df.groupby(pd.Grouper(key='creado_dia',freq='M'))['id'].count()
+y = df_users.groupby(pd.Grouper(key='creado_dia',freq='M'))['id'].count()
 x = y.index
-
-st.write('Data we are going to plot')
-st.write(y)
-
-#Example data visualization using bokeh
 
 p = figure(
      title='New users per month',
@@ -98,8 +29,42 @@ p = figure(
 p.background_fill_color = "#22201F"
 p.line(x,y, line_width=2)
 
-st.write('Chart using bokeh')
+st.subheader('New Users per month')
 st.bokeh_chart(p)
+
+
+
+
+#Analysis per country 
+
+countries_count = df_users.groupby('pais')['id'].count()
+st.subheader('Usuarios por paises')
+st.write(countries_count.sort_values( ascending=False))
+
+
+
+
+#Challenges and community analysis 
+
+challenges_df = pd.read_csv('dummy_data.csv')
+
+popular_post = challenges_df.groupby('discussion-post')['likes'].sum()
+st.subheader('Post más populares')
+st.bar_chart(data = popular_post)
+
+popular_topics = challenges_df.groupby('Tema challenges')['Challenges completados '].sum()
+st.subheader('Temas más populares')
+st.bar_chart(data=popular_topics)
+
+df_gender = pd.read_csv('gender_mock_data.csv')
+gender_analysis = (df_gender.groupby('genero').count() / df_gender.groupby('genero').count().sum()) * 100
+
+st.write('Género')
+st.bar_chart(data=gender_analysis)
+
+
+
+
 
 
 #xx = pd.read_sql_query()
